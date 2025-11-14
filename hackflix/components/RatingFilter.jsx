19b3fe17fd9dcf979693611a274a4b3f1547b5c5
Juan <juanignaciojobs@gmail.com>
2025-11-14 20:13:31 -0300

    feat: Component RatingFilter.jsx initialized and working

--- hackflix/components/RatingFilter.jsx
+++ hackflix/components/RatingFilter.jsx
@@ -1,3 +1,10 @@
+import { Rating } from "react-simple-star-rating";
 export default function RatingFilter() {
   console.log("RatingFilter component rendered");
+  return (
+    <div className="rating-filter">
+      <label htmlFor="rating">Filter by Rating:</label>
+      <Rating></Rating>
+      </div>
+  );
 }
